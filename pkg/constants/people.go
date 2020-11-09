d18c9b94e5dc62ea37aa17b8aac4f3f3e21e697e
s.borzenkova <ssborzenkova@edu.hse.ru>
2020-11-09 23:59:43 +0300

    change people list

--- pkg/constants/people.go
+++ pkg/constants/people.go
@@ -2,19 +2,28 @@ package constants
 
 var (
 	People = []string{
+		"kabobrov",
+		"an_Sasch",
+		"tamara_vedenina",
 		"Sonichka1311",
-		"KositsynPA",
-		"TwoBlueCats",
+		"MichaelNiki",
+		"Max143672",
 	}
 	LastPersonNum = 0
 	TgToGitlab    = map[string]string{
-		"Sonichka1311": "Sonichka1311",
-		"KositsynPA": "Vemmy",
-		"TwoBlueCats": "asanikushin",
+		"kabobrov":        "k.bobrov",
+		"an_Sasch":        "aandreev.usetech",
+		"tamara_vedenina": "t.vedenina",
+		"Sonichka1311":    "Sonichka1311",
+		"MichaelNiki":     "MishaNiki",
+		"Max143672":       "max143672",
 	}
-	GitlabToTg    = map[string]string{
-		"Sonichka1311": "Sonichka1311",
-		"Vemmy": "KositsynPA",
-		"asanikushin":"TwoBlueCats",
+	GitlabToTg = map[string]string{
+		"k.bobrov":         "kabobrov",
+		"aandreev.usetech": "an_Sasch",
+		"t.vedenina":       "tamara_vedenina",
+		"Sonichka1311":     "Sonichka1311",
+		"MishaNiki":        "MichaelNiki",
+		"max143672":        "Max143672",
 	}
 )
